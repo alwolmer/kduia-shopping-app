170477a0e3ddc057d2b0646a90c983b722823f9b
alwolmer <arthur.wolmer@gmail.com>
2024-06-18 20:31:27 -0300

    added ItemSelected and Location components

--- src/components/ItemSelected.js
+++ src/components/ItemSelected.js
@@ -2,6 +2,58 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const ItemSelected = (props) => {
+    const {dispatch, expenses} = useContext(AppContext);
+
+    const [name, setName] = useState('');
+    const [quantity, setQuantity] = useState('');
+    const [action, setAction] = useState('');
+
+    const submitEvent = () => {
+        const item = {
+            name: name,
+            quantity: parseInt(quantity),
+        };
+
+        dispatch({
+            type: action,
+            payload: item,
+        });
+    };
+
+    return (
+        <div>
+            <div className='row'>
+                <div className='input-group mb-3' style={{marginLeft: '2rem'}}>
+                    <div className='input-group-prepend'>
+                        <label className='input-group-text' htmlFor='inputGroupSelect01'>Items</label>
+                    </div>
+                    <select className='custom-select' id='inputGroupSelect01' onChange={(event) => setName(event.target.value)}>
+                        <option defaultValue>Choose...</option>
+                        {expenses.map((expense) => (
+                            <option value={expense.name} name={expense.name}>{expense.name}</option>
+                        ))}
+                    </select>
+                    <div className='input-group-prepend' style={{marginLeft: '2rem'}}>
+                        <label className='input-group-text' htmlFor='inputGroupSelect02'>Quantity</label>
+                    </div>
+                        <select className='custom-select' id='inputGroupSelect01' onChange={(event) => setName(event.target.value)}>
+                            <option defaultValue value='ADD_QUANTITY' name='Add'>Add</option>
+                            <option value='RED_QUANTITY' name='Reduce'>Reduce</option>
+                        </select>
+                        <span className="eco" style={{ marginLeft: '2rem', marginRight: '8px'}}></span>
+                        <input
+                            required
+                            type='number'
+                            id='cost'
+                            value={quantity}
+                            style={{size: 10}}
+                            onChange={(event) => setQuantity(event.target.value)}>
+                        </input>
+                        <button className='btn btn-primary' onClick={submitEvent} style={{marginLeft: '2rem'}}>Save</button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ItemSelected;
--- src/components/Location.js
+++ src/components/Location.js
@@ -2,6 +2,25 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Location = () => {
+    const {dispatch} = useContext(AppContext);
+
+    const changeLocation = (newLoc) => {
+        dispatch({
+            type: 'CHG_LOCATION',
+            payload: newLoc,
+        });
+    }
+
+    return (
+        <div className='alert alert-secondary'>
+            <select name="Location" id="Location" onChange={event=>changeLocation(event.target.value)}>
+                <option value="£">Uk(£)</option>
+                <option value="₹">India(₹)</option>
+                <option value="€">Europe(€)</option>
+                <option value="CAD">Canada(CAD)</option>
+            </select>	
+        </div>
+    );
 };
 
 export default Location;
